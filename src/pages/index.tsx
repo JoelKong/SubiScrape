5090bf107aa6798d08915946562eddd8d8e10a9b
Joel Kong <joelkong2001@gmail.com>
2023-03-28 14:35:25 +0800

    feat: imported MUI and setup dark theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import { Inter } from "next/font/google";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const inter = Inter({ subsets: ["latin"] });
 //inter.className
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export default function Home() {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <div>hi</div>
-    </>
+    </ThemeProvider>
   );
 }
