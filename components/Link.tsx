befb3446fa64fea59f033fcb5a3c415d08647673
Joel Kong <joelkong2001@gmail.com>
2023-03-29 20:27:49 +0800

    feat: styled button

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,22 +2,33 @@ import classes from "./link.module.css";
 import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 function Link(): JSX.Element {
   return (
     <section className={classes.section}>
       <p>Input the link that you wish to summarise information from.</p>
-      <form className={classes.textfield}>
+      <form className={classes.form}>
         <TextField
           id="outlined-basic"
           label="Link"
           variant="outlined"
           placeholder="E.g. medium.com"
+          size="small"
           fullWidth
         />
-        <Button variant="contained" endIcon={<SendIcon />}>
-          Go
-        </Button>
+
+        <LoadingButton
+          className={classes.button}
+          // onClick={handleClick}
+          endIcon={<SendIcon />}
+          // loading
+          // ={loading}
+          loadingPosition="end"
+          variant="contained"
+        >
+          <span>Go</span>
+        </LoadingButton>
       </form>
     </section>
   );
