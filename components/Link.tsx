0d9a7d5fdc2b84f58f453377d4ba326283ec80e5
Joel Kong <joelkong2001@gmail.com>
2023-03-29 20:58:36 +0800

    feat: send api request to summarise link

--- components/Link.tsx
+++ components/Link.tsx
@@ -6,12 +6,33 @@ import { useState } from "react";
 
 function Link(): JSX.Element {
   const [inputLink, setInputLink] = useState<String>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  async function submitLink(e: React.FormEvent) {
+    e.preventDefault();
+    if (error) setError(false);
+
+    if (!inputLink) {
+      setError(true);
+      return;
+    }
+
+    const summariseLink = await fetch("/api/summarise", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputLink),
+    });
+  }
 
   return (
     <section className={classes.section}>
       <p>Input the link that you wish to summarise information from.</p>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitLink}>
         <TextField
+          error={error}
           onChange={(e) => setInputLink(e.target.value)}
           value={inputLink}
           id="outlined-basic"
@@ -19,19 +40,23 @@ function Link(): JSX.Element {
           variant="outlined"
           placeholder="E.g. medium.com"
           size="small"
+          helperText={error && "Please input a link"}
           fullWidth
         />
 
         <LoadingButton
+          onClick={submitLink}
           className={classes.button}
-          // onClick={handleClick}
           endIcon={<SendIcon />}
-          // loading
-          // ={loading}
+          loading={loading}
           loadingPosition="end"
           variant="contained"
         >
-          <span>Go</span>
+          {loading ? (
+            <span className={classes.buttonspan}>Loading</span>
+          ) : (
+            <span>Go</span>
+          )}
         </LoadingButton>
       </form>
     </section>
