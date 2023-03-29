c5ebdd5f99a60bf8646a34cd658ab9e98d4cc19c
Joel Kong <joelkong2001@gmail.com>
2023-03-29 20:33:43 +0800

    feat: controlled state for link input

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,15 +1,19 @@
 import classes from "./link.module.css";
 import { TextField } from "@mui/material";
-import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import LoadingButton from "@mui/lab/LoadingButton";
+import { useState } from "react";
 
 function Link(): JSX.Element {
+  const [inputLink, setInputLink] = useState<String>("");
+
   return (
     <section className={classes.section}>
       <p>Input the link that you wish to summarise information from.</p>
       <form className={classes.form}>
         <TextField
+          onChange={(e) => setInputLink(e.target.value)}
+          value={inputLink}
           id="outlined-basic"
           label="Link"
           variant="outlined"
