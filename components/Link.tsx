99b8be6a931b53fd74502c042176adc0e202679f
Joel Kong <joelkong2001@gmail.com>
2023-03-29 20:15:45 +0800

    feat: import textfield mui

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,23 @@
 import classes from "./link.module.css";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
 
 function Link(): JSX.Element {
   return (
     <section className={classes.section}>
       <p>Input the link that you wish to summarise information from.</p>
-      <form>
-        <input />
-        <button></button>
+      <form className={classes.textfield}>
+        <TextField
+          id="outlined-basic"
+          label="Link"
+          variant="outlined"
+          placeholder="E.g. medium.com"
+          fullWidth
+        />
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Go
+        </Button>
       </form>
     </section>
   );
